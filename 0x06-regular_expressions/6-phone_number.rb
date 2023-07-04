c06bb20bdc78f8d6622c9335b0844a8fcc59c232
Ukeremi <ukpono9@gmail.com>
2023-07-04 12:20:39 -0700

    script matches phone number

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-# Script Name: 6-phone_number.rb
-# Match the pattern: a 10-digit phone number
-matched_string = ARGV[0].scan(/^[0-9]{10}$/).join
-
-# Output the matched string
-puts matched_string
+puts ARGV[0].scan(/^h.n$/).join
